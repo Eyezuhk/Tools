da669b0495ff798653eafda3134109995b15ba54
Isaac Fernandes <101341132+Eyezuhk@users.noreply.github.com>
2024-11-21 18:18:14 -0300

    Update TextExplorer.py

--- TextExplorer.py
+++ TextExplorer.py
@@ -1,7 +1,9 @@
 import os
-
-folder_path = r"C:\..."
-search_term = "Text"
+import time
+start_time = time.time()
+	
+folder_path = r"C:\Users\"
+search_term = "TEXT"
 
 total_files = 0
 files_found = 0
@@ -21,7 +23,9 @@ for root, dirs, files in os.walk(folder_path):
         except UnicodeDecodeError:
             #print(f"Não foi possível ler o arquivo: {file_path}")
             pass
-            
+end_time = time.time()
+total_time = end_time - start_time     
+print(f"\nTempo total de execução: {total_time:.2f} segundos")       
 
 print(f"Total de arquivos lidos: {total_files}")
 print(f"Arquivos encontrados com a palavra '{search_term}': {files_found}")
