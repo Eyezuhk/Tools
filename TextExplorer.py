ce21e0e3ef3b84882cd1804635becdeb620c4b13
Isaac Fernandes <101341132+Eyezuhk@users.noreply.github.com>
2024-11-21 10:44:40 -0300

    Create TextExplorer.py

---
+++ TextExplorer.py
@@ -0,0 +1,36 @@
+import os
+
+folder_path = r"path"
+search_term = "Text"
+
+total_files = 0
+files_found = 0
+found_files = []
+
+for root, dirs, files in os.walk(folder_path):
+    for filename in files:
+        file_path = os.path.join(root, filename)
+        total_files += 1
+        #print(f"Lendo arquivo: {file_path}")
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                if search_term in file.read():
+                    files_found += 1
+                    found_files.append(file_path)
+                    #print(f"Arquivo encontrado: {file_path}")
+        except UnicodeDecodeError:
+            #print(f"Não foi possível ler o arquivo: {file_path}")
+            pass
+            
+
+print(f"Total de arquivos lidos: {total_files}")
+print(f"Arquivos encontrados com a palavra 'Powered': {files_found}")
+
+if found_files:
+    print("\nCaminhos dos arquivos encontrados:")
+    for file_path in found_files:
+        print(file_path)
+else:
+    print("Nenhum arquivo encontrado com a palavra 'Powered'.")
+
+print("Pesquisa concluída.")
